8a255ce59332ac760d1b3e2928884572cf2fb61f
Christian Nunciato <cnunciato@rhapsody.com>
2011-05-21 11:38:45 -0700

    Done

--- OnlineImageLibrary.WebUI/Controllers/BrowseController.cs
+++ OnlineImageLibrary.WebUI/Controllers/BrowseController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineImageLibrary.WebUI.OLR; 
+using OnlineImageLibrary.WebUI.SSO;
 
 namespace OnlineImageLibrary.WebUI.Controllers
 {
@@ -10,6 +12,45 @@ namespace OnlineImageLibrary.WebUI.Controllers
     {
         public ActionResult Index()
         {
+            string token = Request.QueryString["token"];
+            string isbn = Request.QueryString["eISBN"];
+
+            if (token != null && isbn != null && token.ToString() != "" && isbn.ToString() != "")
+            {
+                SSOws sso = new SSOws();
+                OLRws olr = new OLRws();
+                SSOwsResult tokenResult = sso.validateToken(token);
+
+                if (tokenResult.resultDescription == "Success")
+                {
+                    getEntitlementParameters parameters = new getEntitlementParameters();
+                    parameters.userToken = token;
+
+                    filter eisbnFilter = new filter();
+                    parameters.userToken = token;
+                    eisbnFilter.filterType = "EISBN";
+                    eisbnFilter.filerValue = isbn;
+                    parameters.filter = eisbnFilter;
+                    OLRwsResult entitlementResult = olr.getEntitlements(parameters);
+
+                    if (entitlementResult.resultDescription == "Success")
+                    {
+                        // All good; do nothing
+                    }
+                    else
+                    {
+                        return View("AccessDenied");
+                    }
+                }
+                else
+                {
+                    return View("AccessDenied");
+                }
+            }
+            else
+            {
+                return View("AccessDenied");
+            }
             return View();
         }
     }
---
+++ OnlineImageLibrary.WebUI/Views/Shared/AccessDenied.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Access Denied</title>
+    <link rel="Stylesheet" href="/Content/App.css" />
+</head>
+<body>
+    <div id="container">
+        <div id="header"></div>
+        <div id="content">
+            <div id="access-denied">
+                <h1>Invalid Request</h1>
+                <p>
+                    Your request could not be processed, either because of an expired session or missing entitlement.
+                    Please sign out and back in again, and if the problem persists, contact an administrator.
+                </p>
+            </div>
+        </div>
+    <div>
+</body>
+</html>
